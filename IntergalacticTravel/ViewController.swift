f9b436e82c4a2d54847514599839b424d5896320
David Siemionko <dsiemionko4406@stu.d214.org>
2021-01-06 15:29:49 -0500

    Initial Commit

---
+++ IntergalacticTravel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IntergalacticTravel
+//
+//  Created by user180860 on 1/6/21.
+//  Copyright © 2021 user180860. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
